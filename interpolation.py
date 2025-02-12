5a363f664822639ffdccf1f1cbb7e97f93d34747
l3anxna <patthadonaroonpairodjanakul@gmail.com>
2025-02-12 21:02:21 +0700

    last step

--- interpolation.py
+++ interpolation.py
@@ -1,33 +1,45 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def F1(x):
+def F(x):
     return np.sin(x)
 
 def F2(x):
-    return np.sin(3 * x)
+    return np.sin(3*x)
 
 def F3(x):
-    return np.sin(5 * x)
+    return np.sin(5*x)
 
-def interpolate_and_plot(F, a, b, m, title):
+def lagrange_interpolation(x_points, y_points, x):
+    n = len(x_points)
+    P = 0
+    for i in range(n):
+        L_i = 1
+        for j in range(n):
+            if i != j:
+                L_i *= (x - x_points[j]) / (x_points[i] - x_points[j])
+        P += y_points[i] * L_i
+    return P
+
+def plot_interpolation(func, a, b, m, title):
     x_points = np.linspace(a, b, m)
-    y_points = F(x_points)
+    y_points = func(x_points)
 
     V = np.vander(x_points, m)
     coeffs = np.linalg.solve(V, y_points)
 
-    def P(x):
+    def P_sle(x):
         return np.polyval(coeffs, x)
 
     x_fine = np.linspace(a, b, 500)
-    f_values = F(x_fine)
-    p_values = P(x_fine)
+    f_values = func(x_fine)
+    p_sle_values = P_sle(x_fine)
+    p_lagrange_values = [lagrange_interpolation(x_points, y_points, xi) for xi in x_fine]
 
-
-    plt.figure()
+    plt.figure(figsize=(8, 6))
     plt.plot(x_fine, f_values, label="Original Function", color="blue")
-    plt.plot(x_fine, p_values, label="Interpolation Polynomial", linestyle="--", color="orange")
+    plt.plot(x_fine, p_sle_values, label="Interpolation Polynomial (SLE)", linestyle="--", color="orange")
+    plt.plot(x_fine, p_lagrange_values, label="Interpolation Polynomial (Lagrange)", linestyle="-.", color="green")
     plt.scatter(x_points, y_points, color="red", label="Interpolation Points")
     plt.legend()
     plt.title(title)
@@ -36,9 +48,13 @@ def interpolate_and_plot(F, a, b, m, title):
     plt.grid()
     plt.show()
 
-a, b = 0, 4  
-m = 5 
+a, b = 0, 4
 
-interpolate_and_plot(F1, a, b, m, "Interpolation of sin(x) on [0, 4]")
-interpolate_and_plot(F2, a, b, m, "Interpolation of sin(3x) on [0, 4]")
-interpolate_and_plot(F3, a, b, m, "Interpolation of sin(5x) on [0, 4]")
+functions = [F, F2, F3]
+num_points = [5, 7, 9]
+
+for func in functions:
+    for m in num_points:
+        func_name = func.__name__
+        title = f"Interpolation of {func_name} on [0, 4] with {m} points"
+        plot_interpolation(func, a, b, m, title)
