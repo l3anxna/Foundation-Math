6cceaa93695b3ff798c6b6d843f34be9bcfb06eb
l3anxna <patthadonaroonpairodjanakul@gmail.com>
2025-02-14 13:50:52 +0700

    draw heart

--- circle.py
+++ circle.py
@@ -7,11 +7,11 @@ r = 3
 
 
 def x(t):
-    return h + r * np.cos(t)
+    return 16 * np.sin(t) ** 3
 
 
 def y(t):
-    return k + r * np.sin(t)
+    return 13 * np.cos(t) - 5 * np.cos(2 * t) - 2 * np.cos(3 * t) - np.cos(4 * t)
 
 
 t_fine = np.linspace(0, 2 * np.pi, 500)
@@ -19,7 +19,8 @@ x_fine = x(t_fine)
 y_fine = y(t_fine)
 
 plt.figure(figsize=(8, 8))
-plt.plot(x_fine, y_fine, label="Circle", color="pink", linewidth=2)
-
+plt.plot(x_fine, y_fine, label="Circle", color="lightcoral", linewidth=2)
+ax = plt.gca()
+ax.set_facecolor("mistyrose")
 
 plt.show()
